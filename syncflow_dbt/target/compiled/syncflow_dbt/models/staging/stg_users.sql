ddc1a15c9134ae46b18e1909da18350dd973bc3c
Amruth Raj <connect.amruth@gmail.com>
2026-01-28 16:47:18 +0000

    docs: added source and stage-level documentation and descriptions

--- syncflow_dbt/target/compiled/syncflow_dbt/models/staging/stg_users.sql
+++ syncflow_dbt/target/compiled/syncflow_dbt/models/staging/stg_users.sql
@@ -5,7 +5,7 @@ with source as (
 renamed as (
     select
         user_id,
-        split_part(email, '@', 2) as email_username,
+        split_part(email, '@', 2) as email_domain,
         company_size,
         signup_at as signed_up_at,
     from source
